c0cfe3670c6e0efdca195e78e22653cd13da5518
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-06-25 16:16:39 -0300

    feat: create react applications with most of important libraries

---
+++ redux-functions/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { DivHomeBase } from "./styles"
+
+const HomePage = () => {
+  return (
+    <DivHomeBase>
+    <h1>Redux Functions Example Project</h1>
+    </DivHomeBase>
+  )
+}
+
+export default HomePage
